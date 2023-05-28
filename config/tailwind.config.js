0723f87b25026ab3417892676648bddb6a642fe2
exit <halimshams8@gmail.com>
2023-05-28 17:42:10 +0430

    responsiveness added

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -9,6 +9,25 @@ module.exports = {
     './app/views/**/*.{erb,haml,html,slim}'
   ],
   theme: {
+    screens: {
+      'xsm': '588px',
+      // => @media (min-width: 640px) { ... }
+
+      'sm': '700px',
+      // => @media (min-width: 860px) { ... }
+
+      'md': '860px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
